b23953b24887811d9c95cb694921f99a90ad835c
Daniel Thielking <thielking@ma102.int.modac.eu>
2016-09-27 08:46:50 +0200

    Juse spaces instead of tab characters

--- publish.sh
+++ publish.sh
@@ -7,9 +7,9 @@
 ## Date:    09/26/2016
 
 # Absolute path this script is in. /home/user/bin
-REPOPATH=`dirname $(readlink -f $0)`					# Set absolut path to script directory
-DOT_FILES_DIR="$REPOPATH/files"							# Set files directory
-DOT_BACKUP_DIR="$HOME/dots_backup"						# Set Backup directory
+REPOPATH=`dirname $(readlink -f $0)`    # Set absolut path to script directory
+DOT_FILES_DIR="$REPOPATH/files"         # Set files directory
+DOT_BACKUP_DIR="$HOME/dots_backup"      # Set Backup directory
 
 
 echo "Doing backups of your current files"
@@ -18,21 +18,19 @@ echo "you will find your old files in ${DOT_BACKUP_DIR}"
 cd $DOT_FILES_DIR
 
 for DOT_FILE in `ls`
-	do
-		# Backup function
-		if [ ! -d ${DOT_BACKUP_DIR} ]
-		then
-			mkdir ${DOT_BACKUP_DIR}
-		fi
-		if [ -f ${HOME}/.${DOT_FILE} ]
-		then
-			echo "Backup: .${DOT_FILE}"
-			mv ${HOME}/.${DOT_FILE} ${DOT_BACKUP_DIR}/${DOT_FILE}.bak
-		fi
+do
+    # Backup function
+    if [ ! -d ${DOT_BACKUP_DIR} ]
+    then
+        mkdir ${DOT_BACKUP_DIR}
+    fi
 
-		if [ -f ${DOT_BACKUP_DIR}/${DOT_FILE}.bak ]
-		then
-			# Deployment function
-			cp ${DOT_FILES_DIR}/${DOT_FILE} ${HOME}/.${DOT_FILE}
-		fi
+    if [ -f ${HOME}/.${DOT_FILE} ]
+    then
+        echo "Backup: .${DOT_FILE}"
+        mv ${HOME}/.${DOT_FILE} ${DOT_BACKUP_DIR}/${DOT_FILE}.bak
+    fi
+
+    # Deployment function
+    cp ${DOT_FILES_DIR}/${DOT_FILE} ${HOME}/.${DOT_FILE}
 done
