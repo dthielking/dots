49e8ea79ed002769fb69f4460f8380afc1c3c831
dthielking <github@thielking-vonessen.de>
2016-09-30 07:43:40 +0200

    Add possibility to make decision to update/upgrade/install with options

--- publish.sh
+++ publish.sh
@@ -11,26 +11,41 @@ REPOPATH=`dirname $(readlink -f $0)`    # Set absolut path to script directory
 DOT_FILES_DIR="$REPOPATH/files"         # Set files directory
 DOT_BACKUP_DIR="$HOME/dots_backup"      # Set Backup directory
 
-
-echo "Doing backups of your current files"
-echo "you will find your old files in ${DOT_BACKUP_DIR}"
-
-cd $DOT_FILES_DIR
-
-for DOT_FILE in `ls`
+while getopts iush option
 do
-    # Backup function
-    if [ ! -d ${DOT_BACKUP_DIR} ]
-    then
-        mkdir ${DOT_BACKUP_DIR}
-    fi
-
-    if [ -f ${HOME}/.${DOT_FILE} ]
-    then
-        echo "Backup: .${DOT_FILE}"
-        mv ${HOME}/.${DOT_FILE} ${DOT_BACKUP_DIR}/${DOT_FILE}.bak
-    fi
-
-    # Deployment function
-    cp ${DOT_FILES_DIR}/${DOT_FILE} ${HOME}/.${DOT_FILE}
+    case $option in
+        i)  rollout;;
+        u)  cd $REPOPATH && git pull && rollout;;
+        s)  cd $REPOPATH && git status;;
+        h|*)  echo "HELP:"
+            echo "\t-i install local files"
+            echo "\t-u to upgrade all dotfiles"
+            echo "\t-s to check for updates"
+            echo "\t-h for help"
+    esac
 done
+
+rollout() {
+    echo "Doing backups of your current files"
+    echo "you will find your old files in ${DOT_BACKUP_DIR}"
+
+    cd $DOT_FILES_DIR
+
+    for DOT_FILE in `ls`
+    do
+        # Backup function
+        if [ ! -d ${DOT_BACKUP_DIR} ]
+        then
+            mkdir ${DOT_BACKUP_DIR}
+        fi
+
+        if [ -f ${HOME}/.${DOT_FILE} ]
+        then
+            echo "Backup: .${DOT_FILE}"
+            mv ${HOME}/.${DOT_FILE} ${DOT_BACKUP_DIR}/${DOT_FILE}.bak
+        fi
+
+        # Deployment function
+        cp ${DOT_FILES_DIR}/${DOT_FILE} ${HOME}/.${DOT_FILE}
+    done
+}
