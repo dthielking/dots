fca0b7a2ba0592a3ef830ce887493a8acd6c6f01
dthielking <github@thielking-vonessen.de>
2016-12-09 11:05:13 +0100

    Removed Vundle from vim conf

--- publish.sh
+++ publish.sh
@@ -3,28 +3,29 @@
 ## Purpose: Publish all dotfiles into appropriate directories
 ## Author:  Daniel Thielking
 ## Mail:    github@thielking-vonessen.de
-## Version: 0.1
+## Version: 0.2
 ## Date:    09/26/2016
 
-# Absolute path this script is in. /home/user/bin
+# Absolute path this script is in. /home/$USERNAME/dots
 REPOPATH=`dirname $(readlink -f $0)`    # Set absolut path to script directory
 DOT_FILES_DIR="$REPOPATH/files"         # Set files directory
 DOT_BACKUP_DIR="$HOME/dots_backup"      # Set Backup directory
 
 rollout() {
-    echo "Doing backups of your current files"
-    echo "you will find your old files in ${DOT_BACKUP_DIR}"
-
-    cd $DOT_FILES_DIR
+    cd ${DOT_FILES_DIR}
 
     for DOT_FILE in `ls`
     do
         # Deployment function
-        cp -r ${DOT_FILES_DIR}/${DOT_FILE} ${HOME}/.${DOT_FILE} 2> /dev/null
+        cp -r -f ${DOT_FILES_DIR}/${DOT_FILE} ${HOME}/.${DOT_FILE} 2> /dev/null
     done
 }
 
 backup () {
+    echo "Doing backups of your current dot files"
+    echo "you will find your old dot files in ${DOT_BACKUP_DIR}"
+
+    cd ${DOT_FILES_DIR}
 
     for DOT_FILE in `ls`
     do
@@ -34,10 +35,10 @@ backup () {
             mkdir ${DOT_BACKUP_DIR}
         fi
 
-        if [ -f ${HOME}/.${DOT_FILE} ]
+        if [ -e ${HOME}/.${DOT_FILE} ]
         then
             echo "Backup: .${DOT_FILE}"
-            mv ${HOME}/.${DOT_FILE} ${DOT_BACKUP_DIR}/${DOT_FILE}.bak
+            cp -r -f ${HOME}/.${DOT_FILE} ${DOT_BACKUP_DIR}/${DOT_FILE}.$(date +%Y%m%d%H%M%S).bak
         fi
     done
 }
@@ -54,7 +55,7 @@ while [ $# -gt 0 ]
 do
     case $1 in
         -b)  backup;;
-        -i)  rollout;;
+        -i)  backup && rollout;;
         -u)  cd $REPOPATH && git pull && rollout;;
         -s)  cd $REPOPATH && git status;;
         -h|-*|*)  usage && return;;
