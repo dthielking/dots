1cc3993dbec7e66025833f33da616c1741d84345
Daniel Thielking <vonessen@vonessen.local>
2019-05-06 15:22:39 +0200

    Added virtualenvwrapper for MAC.

---
+++ files/zshrc_custom/python_env_setup.sh
@@ -0,0 +1,22 @@
+# activate virtualenvwrapper
+if [ -x /usr/local/bin/virtualenvwrapper.sh ]
+    then
+        export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+        source /usr/local/bin/virtualenvwrapper.sh
+
+        # pip should only run if there is a virtualenv currently activated
+        export PIP_REQUIRE_VIRTUALENV=true
+
+        # create commands to override pip restriction.
+        # use `gpip` or `gpip3` to force installation of
+        # a package in the global python environment
+        gpip(){
+           PIP_REQUIRE_VIRTUALENV="" pip "$@"
+        }
+        gpip3(){
+           PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
+        }
+else
+        echo "virtualenvwrapper.sh missing"
+        echo "Install it via: pip install virtualenvwrapper"
+fi
